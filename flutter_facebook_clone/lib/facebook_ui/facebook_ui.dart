e4fcd9a1eaaec78a50e38a064af195aca9af0f49
Martin <martin.e.castro.88@gmail.com>
2023-07-07 14:04:52 -0300

    appbar created

--- flutter_facebook_clone/lib/facebook_ui/facebook_ui.dart
+++ flutter_facebook_clone/lib/facebook_ui/facebook_ui.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_facebook_clone/facebook_ui/widgets/cicle_button.dart';
+import 'package:flutter_facebook_clone/icons/custom_icons.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FacebookUI extends StatelessWidget {
   const FacebookUI({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.light,
+        ),
+        leadingWidth: 150,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: SvgPicture.asset('assets/logos/facebook.svg',
+        color: Colors.blueAccent,
+        width:150,),
+        actions: const [
+          CicleButton(color: Color(0xffBFBFBF), iconData: CustomIcons.search),
+          SizedBox(width: 15),
+          CicleButton(color: Color(0xffFE7574), iconData: CustomIcons.bell),
+          SizedBox(width: 15),
+          CicleButton(color: Color(0xff7BBAFF), iconData: CustomIcons.user_friends, showBadge: true,),
+          SizedBox(width: 15),
+          CicleButton(color: Color(0xff4C86E4), iconData: CustomIcons.messenger),
+          SizedBox(width: 15),
+        ],
+        ),
+    );
   }
 }
