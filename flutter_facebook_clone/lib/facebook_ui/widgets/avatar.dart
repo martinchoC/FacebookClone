371ed26b35bf2acaf0370335538dfc39ca6e5067
Martin <martin.e.castro.88@gmail.com>
2023-07-26 16:32:52 -0300

    Update avatar.dart

--- flutter_facebook_clone/lib/facebook_ui/widgets/avatar.dart
+++ flutter_facebook_clone/lib/facebook_ui/widgets/avatar.dart
@@ -3,19 +3,25 @@ import 'package:flutter/material.dart';
 class Avatar extends StatelessWidget {
   final double size;
   final String asset;
-  const Avatar({super.key, required this.size, required this.asset});
+  final double borderWidth;
+  const Avatar(
+      {super.key,
+      required this.size,
+      required this.asset,
+      required this.borderWidth});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(size * 0.5),
-        child: Image.asset(
-          asset,
-          width: size,
-          height: size,
-        ),
-      ),
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            width: borderWidth,
+            color: Colors.white,
+          ),
+          image: DecorationImage(image: AssetImage(asset))),
     );
   }
 }
