2295669e96e13a5678310305ebb47d1ec8c9cedd
Martin <martin.e.castro.88@gmail.com>
2023-07-26 16:33:05 -0300

    Create stories.dart

---
+++ flutter_facebook_clone/lib/facebook_ui/widgets/stories.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_clone/facebook_ui/widgets/story_item.dart';
+import 'package:flutter_facebook_clone/models/story.dart';
+
+final _stories = [
+  Story(
+    bg: 'assets/wallpapers/1.jpeg',
+    avatar: 'assets/users/1.jpg',
+    username: 'Laura',
+  ),
+  Story(
+    bg: 'assets/wallpapers/2.jpeg',
+    avatar: 'assets/users/2.jpg',
+    username: 'Pepe',
+  ),
+  Story(
+    bg: 'assets/wallpapers/3.jpeg',
+    avatar: 'assets/users/3.jpg',
+    username: 'Lili',
+  ),
+  Story(
+    bg: 'assets/wallpapers/4.jpeg',
+    avatar: 'assets/users/4.jpg',
+    username: 'Baley',
+  ),
+  Story(
+    bg: 'assets/wallpapers/5.jpeg',
+    avatar: 'assets/users/5.jpg',
+    username: 'Mario',
+  ),
+];
+
+class Stories extends StatelessWidget {
+  const Stories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 160,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (_, index) {
+            final story = _stories[index];
+            return StoryItem(
+              story: story,
+              isFirst: index == 0,
+            );
+          },
+        ));
+  }
+}
